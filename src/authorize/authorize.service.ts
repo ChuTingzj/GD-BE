ce76ce19ef37960489e99312885bccad05e5e379
zhangjing <zhangjing@qianjiangcloud.com>
2023-02-01 18:02:04 +0800

    test: github authorization

--- src/authorize/authorize.service.ts
+++ src/authorize/authorize.service.ts
@@ -1,5 +1,7 @@
+import fetch from 'node-fetch';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
+
 @Injectable()
 export class AuthorizeService {
   constructor(private readonly prismaService: PrismaService) {}
@@ -8,6 +10,6 @@ export class AuthorizeService {
     const res = await fetch(
       `https://github.com/login/oauth/access_token?client_id=983cf4b3feff31bba087&client_secret=7a20564d4c78a7bb055f8cde2b1b25a07d5a5a32&code=${code}`,
     );
-    console.log(res);
+    console.log(res.text());
   }
 }

0ba6f49ba0943d465e29c78afae0020244c8cb1f
zhangjing <zhangjing@qianjiangcloud.com>
2023-02-02 13:33:16 +0800

    test: github authorization

--- src/authorize/authorize.service.ts
+++ src/authorize/authorize.service.ts
@@ -9,33 +9,33 @@ import type { GithubUserInfo } from '@/types';
 export class AuthorizeService {
   constructor(private readonly prismaService: PrismaService) {}
   async getToken(code: string, res: Response) {
+    const acceptHeaders = new Headers({
+      Accept: 'application/json',
+    });
     try {
-      const acceptHeaders = new Headers({
-        Accept: 'application/json',
-      });
       const res_token = await fetch(
         `https://github.com/login/oauth/access_token`,
         {
           method: 'POST',
-          body: {
+          body: JSON.stringify({
             client_id: '983cf4b3feff31bba087',
             client_secret: '7a20564d4c78a7bb055f8cde2b1b25a07d5a5a32',
             code,
-          },
+          }),
           headers: acceptHeaders,
         },
       );
       const json = await res_token.json();
-      console.log(json);
-      const token = json.access_token;
-      const headers = new Headers({
-        Authorization: `Bearer ${token}`,
-      });
-      const res_userInfo = await fetch(`https://api.github.com/user`, {
-        headers,
-      });
-      const json_userInfo: GithubUserInfo = await res_userInfo.json();
-      console.log(json_userInfo);
+      console.log(29, json);
+      // const token = json.access_token;
+      // const headers = new Headers({
+      //   Authorization: `Bearer ${token}`,
+      // });
+      // const res_userInfo = await fetch(`https://api.github.com/user`, {
+      //   headers,
+      // });
+      // const json_userInfo: GithubUserInfo = await res_userInfo.json();
+      // console.log(json_userInfo);
     } catch (e) {
       res.send(e.message);
     }
